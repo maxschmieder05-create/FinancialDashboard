e861444dab0a487d2aac32bf1aaad76eec5f9194
Max Schmieder <maxschmieder@Maxs-MacBook-Air.local>
2026-05-09 20:46:13 -0400

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production"
+
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: isProd ? "/FinancialDashboard" : "",
   typescript: {
     ignoreBuildErrors: true,
   },
